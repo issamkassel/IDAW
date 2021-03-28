47eb18fb2d0289e4051e5f80d3d4661644686f0c
Mohammed Issam Kassel <kassel.issam@gmail.com>
2021-03-28 13:33:21 +0200

    Cookies

---
+++ TP3/cookies/index.php
@@ -0,0 +1,29 @@
+<?php
+    $style = 'style1';
+    if (isset($_COOKIE['style']))
+        $style = $_COOKIE['style'];
+    if(isset($_POST['css'])){
+        $style = $_POST['css'];
+        setcookie('style',$style);
+    }
+?>
+
+<!doctype html>
+<html>
+    <head>
+        <title></title>
+        <?php
+            echo "<link rel=\"stylesheet\" href=\"$style.css\" type=\"text/css\"
+        media=\"screen\" title=\"default\" charset=\"utf-8\" />";
+        ?>
+    </head>
+    <body>
+        <form id="style_form" action="index.php" method="POST">
+            <select name="css">
+                <option value="style1">style1</option>
+                <option value="style2">style2</option>
+            </select>
+            <input type="submit" value="Appliquer" />
+        </form>
+    </body>
+</html>
